8caf9c9a3faa0e946e6fa3d838d5a7dcdc29c9b9
rezenter <nisovru@gmail.com>
2014-09-14 10:09:25 +0400

    hw

---
+++ first.py
@@ -0,0 +1,38 @@
+__author__ = 'rezenter'
+
+l = [1, 2, 2, 3]
+l1 = [1, 3, 5, 7, 8]
+l2 = [2, 4, 6]
+
+
+def remove_adjacent(lst):
+    res = []
+    prev = None
+    for i in range(0, len(lst)):
+        tmp = lst.pop()
+        if tmp != prev:
+            res.append(tmp)
+            prev = tmp
+    res.reverse()
+    return res
+print(remove_adjacent(l))
+
+def linear_merge(lst1, lst2):
+    lst = []
+    i = 0
+    while len(lst1) != 0 and len(lst2) != 0:
+        t1 = lst1.pop(i)
+        t2 = lst2.pop(i)
+        if t1 < t2:
+            lst.append(t1)
+            lst.append(t2)
+        else:
+            lst.append(t2)
+            lst.append(t1)
+        i+1
+    if len(lst1) != 0:
+        lst.extend(lst1)
+    else:
+        lst.extend(lst2)
+    return lst
+print(linear_merge(l1, l2))
